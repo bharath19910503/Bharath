84872c626d78305e2b817c237d8e3b3f28158b00
bharath19910503 <146177700+bharath19910503@users.noreply.github.com>
2023-09-27 22:35:45 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,17 +1,23 @@
 <?php
-//get data from form  
-$name = $_POST['name'];
-$PhoneNumber = $_POST['Mobile'];
-$email= $_POST['emailaddress'];
-$message-box= $_POST['Message'];
-$to = "varshithinteriorsolutions@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name .  "\r\n  Mobile = " . $PhoneNumber ."\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:thankyou.html");
-?>
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message =$_POST["message"];
+require "vendor/autoload.php";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\STMP;
+$mail = new PHPMailer(true);
+                                     
+$mail->isSMTP();
+$mail->SMTPAUTH = TRUE;
+$mail->Host       = "smtp.example.com;";  
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port       = 587; 
+$mail->Username   = "varshithinteriorsolutions@gmail.com";                
+    $mail->Password   = "Sab@1991";  
+    $mail->setFrom($email, $name);  
+    $mail->addAddress("bharath@example.com", "Bharath");
+    $mail->Subject = $Subject;
+    $mail->Body    = $message;
+    $mail->send();
+    echo "Mail has been sent successfully!";
