6f132905fe1f5505a02ddef7844c8a5e34b81dff
bharath19910503 <146177700+bharath19910503@users.noreply.github.com>
2023-09-27 21:57:37 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
 //get data from form  
 $name = $_POST['name'];
-$Phone Number = $_POST['Mobile'];
+$PhoneNumber = $_POST['Mobile'];
 $email= $_POST['emailaddress'];
 $message-box= $_POST['Message'];
 $to = "varshithinteriorsolutions@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name .  "\r\n  Mobile = " . $Phone Number ."\r\n  Email = " . $email . "\r\n Message =" . $message;
+$txt ="Name = ". $name .  "\r\n  Mobile = " . $PhoneNumber ."\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
